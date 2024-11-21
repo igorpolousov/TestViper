b351534849098322402ef97b3132f6f157568f3a
Igor Polousov <igorpolousov@gmail.com>
2024-11-21 15:32:46 +0300

    + main screen

---
+++ VIPERTest/WelcomeScreen/WelcomeScreenViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  VIPERTest
+//
+//  Created by Igor Polousov on 21.11.2024.
+//
+
+import UIKit
+
+class WelcomeScreenViewController: UIViewController {
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
